345c80ded177e0cb99776e7e9c59b69ce9285756
Sam20B <wafulabibiana93@gmail.com>
2024-05-01 13:28:02 +0300

    signin page

---
+++ animeCite.js
@@ -0,0 +1,9 @@
+function welcome() {
+    let message = ("Welcome to my Anime cite :)");
+    alert(message);
+}
+function signIn() {
+    // Navigate to another page within the same domain
+    window.location.href = "signIn.html";
+
+}
