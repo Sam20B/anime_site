bd7a4124618a93613ce91afe6190b5c8cd470227
Sam20B <wafulabibiana93@gmail.com>
2024-05-03 09:21:07 +0300

    the stressing bit is finally done with

--- signIn.js
+++ signIn.js
@@ -1,13 +1,3 @@
 function focusInput() {
-    const inputContainer = document.querySelector(".cont");
-    const inputField = document.getElementById(".i1");
-    const inputLabel = document.querySelector(".l1");
-    
-    inputContainer.classList.add("clicked");
-    
-    if (inputField.value.trim() !== '') {
-      inputLabel.style.top = "7%";
-      inputLabel.style.fontSize = "18px";
-    }
-  }
-  
+  document.getElementById('username').focus();
+}
